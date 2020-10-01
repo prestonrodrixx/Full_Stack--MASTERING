1b6a6ada144fdf22b43e69428b62e4572b5949e4
prestuuu <47060195+prestuuu@users.noreply.github.com>
2020-10-01 18:03:01 +0530

    Roiuting Requests

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
+  const url = req.url;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+    res.write("</html>");
+    return res.end();
+  }
+
+  //   console.log(req.url, req.method, req.headers);
   //   process.exit();
   res.setHeader("Content-Type", "text/html");
-  res.write("<htrml>");
+  res.write("<html>");
   res.write("<head><title>Hello Preston</title></head>");
   res.write("<body><h1>My Preston</h1></body>");
   res.write("</html>");
