4fc703fa3d5040a75c0fbc52c14164cc4570599d
prestuuu <47060195+prestuuu@users.noreply.github.com>
2020-10-04 19:21:55 +0530

    Parsing Incoming Requests (Added body-parser)

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
+const { text } = require("body-parser");
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.use("/", (req, res, next) => {
-  console.log("Hello Preston");
+//   console.log("Hello Preston");
   next();
 });
 
 app.use("/add-product", (req, res, next) => {
-  res.send("<h1>Product Page</h1>");
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+app.use("/product", (req, res, next) => {
+    console.log(req.body);
+    res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
