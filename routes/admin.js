7e70ab75fadba0a3fe00f8b5a728f14cd267cb08
prestuuu <47060195+prestuuu@users.noreply.github.com>
2020-10-05 14:30:45 +0530

    Added 404 Error Page

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,16 @@
- const express = require("express");
+const express = require("express");
 
- const router = express.Router();
+const router = express.Router();
 
- router.get("/add-product", (req, res, next) => {
-    res.send(
-      '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-    );
-  });
-  
-  router.post("/product", (req, res, next) => {
-    console.log(req.body);
-    res.redirect("/");
-  });
+router.get("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+  );
+});
 
- module.exports = router;
+router.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-    res.send("<h1>Shop Page</h1>");
-  });
+  res.send("<h1>Shop Page</h1>");
+});
 
 module.exports = router;
