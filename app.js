b1b1c2d353ec5870eff078ba0cc492671a3aeea7
prestuuu <47060195+prestuuu@users.noreply.github.com>
2020-10-05 18:17:14 +0530

    Serving HTML Pages

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ const shopRoutes = require("./routes/shop");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found.');
-})
+  res.status(404).send("<h1>Page not found.");
+});
 
 app.listen(3000);
