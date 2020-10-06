f9ef8d86b30eb6ab5a4b52e0cb3f78cca9a4393f
prestuuu <47060195+prestuuu@users.noreply.github.com>
2020-10-06 23:44:31 +0530

    Service Files Statically [IMPORTANT]

--- app.js
+++ app.js
@@ -2,18 +2,21 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 
+const rootDir = require("./util/path");
+
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(rootDir , "public")));
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
 });
 
 app.listen(3000);
