6e99bfa691aa0fcf6aabd841f63cc07a2f0fa998
prestuuu <47060195+prestuuu@users.noreply.github.com>
2020-10-08 23:14:40 +0530

    Sharing Data Across Requests | Logging into Const

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 const rootDir = require("./util/path");
 
-const adminRoutes = require("./routes/admin");
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const app = express();
@@ -12,7 +12,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(rootDir , "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
