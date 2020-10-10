f663c3764e770eef6079a3871c6ab118cdf7c25e
prestuuu <47060195+prestuuu@users.noreply.github.com>
2020-10-10 17:24:35 +0530

    Converting HTML files to PUG

--- app.js
+++ app.js
@@ -13,13 +13,13 @@ app.set("view engine", "pug");
 app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(rootDir , "public")));
+app.use(express.static(path.join(rootDir, "public")));
 
 app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
+  res.status(404).render("404", { pageTitle: "Error" });
 });
 
 app.listen(3000);
