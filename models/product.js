2e35e97a94dd5e33f1d90996c48ac5cc0a32927c
prestuuu <47060195+prestuuu@users.noreply.github.com>
2020-10-16 13:08:32 +0530

    Storing Data in Files & Fetching Data via Model

--- models/product.js
+++ models/product.js
@@ -1,4 +1,8 @@
-const products = [];
+const { json } = require("body-parser");
+const fs = require("fs");
+const path = require("path");
+
+// const products = [];
 
 module.exports = class Product {
   constructor(t) {
@@ -6,10 +10,35 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    const p = path.join(
+      path.dirname(require.main.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
   }
 
-  static fetchAll() {
-      return products;
+  static fetchAll(cb) {
+    const p = path.join(
+      path.dirname(require.main.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        cb([]);
+      }
+      // will convert products.json to array
+      cb(JSON.parse(fileContent));
+    });
   }
 };
