17cd32afef999ea0eec5cfb2ad7372aa29a5360b
prestuuu <47060195+prestuuu@users.noreply.github.com>
2020-11-10 11:10:53 +0530

    Working on Navigation

--- routes/shop.js
+++ routes/shop.js
@@ -10,4 +10,8 @@ const router = express.Router();
 
 router.get("/", productsController.getProducts);
 
+router.get("/products");
+router.get("/cart");
+router.get("/checkout");
+
 module.exports = router;
