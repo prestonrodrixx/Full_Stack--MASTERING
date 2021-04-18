1dfaab5522034eba537344fad8c5f08020274f5a
prestuuu <47060195+prestuuu@users.noreply.github.com>
2021-04-18 11:38:55 +0530

    18th April 2021

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-const http = require("http");
-const routes = require('./routes');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-console.log(routes.someText);
+const app = express();
 
-const server = http.createServer(routes.handler);
+app.use(bodyParser.urlencoded({extended: false}));
 
-server.listen(8080);
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></input></form>');
+});
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>Hello from Express</h1>');
+});
+
+
+app.listen(8080);
