629f95eeda4771fb8f2684bd6b9a83714d1a8a91
prestuuu <47060195+prestuuu@users.noreply.github.com>
2021-04-19 09:27:30 +0530

    19th April 2021

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></input></form>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello from Express</h1>');
-});
 
 
 app.listen(8080);
