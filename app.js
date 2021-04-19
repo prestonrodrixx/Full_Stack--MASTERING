2d66f39d28f35fadc7adfbc0f849424e05935ec1
prestuuu <47060195+prestuuu@users.noreply.github.com>
2021-04-19 11:18:02 +0530

    Created HTML Pages

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -8,9 +10,11 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
 
 app.listen(8080);
