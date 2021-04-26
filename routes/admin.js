d7722476a39f78a874df47f958aeb040417ba415
prestuuu <47060195+prestuuu@users.noreply.github.com>
2021-04-26 15:32:34 +0530

    Styling HTML Pages

--- routes/admin.js
+++ routes/admin.js
@@ -2,10 +2,12 @@ const path = require('path');
 
 const express = require('express');
 
+const roodDir = require('../util/path');
+
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+    res.sendFile(path.join(roodDir, 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,12 @@ const path = require('path');
 
 const express = require('express');
 
+const roodDir = require('../util/path');
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+    res.sendFile(path.join(roodDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
