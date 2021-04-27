51d78c8e85d9ac5d23181efc50a42f9ac620936e
prestuuu <47060195+prestuuu@users.noreply.github.com>
2021-04-27 09:36:07 +0530

    HTML & CSS Styling as per instructor

--- util/path.js
+++ util/path.js
@@ -1,3 +1,7 @@
+// Old Depreciated
+// const path = require('path');
+// module.exports = path.dirname(process.mainModule.filename);
+
 const path = require('path');
 
-module.exports = path.dirname(process.mainModule.filename);
+module.exports = path.dirname(require.main.filename);
