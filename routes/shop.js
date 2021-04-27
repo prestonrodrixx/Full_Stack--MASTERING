4b473d60d8733f0729ecb29f2879d14a466ac809
prestuuu <47060195+prestuuu@users.noreply.github.com>
2021-04-27 14:06:18 +0530

    Refactoring

--- routes/shop.js
+++ routes/shop.js
@@ -2,14 +2,21 @@ const path = require('path');
 
 const express = require('express');
 
-const roodDir = require('../util/path');
+const rootDir = require('../util/path');
 const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    console.log(adminData.products);
-    res.sendFile(path.join(roodDir, 'views', 'shop.html'));
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 });
 
 module.exports = router;
