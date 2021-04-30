7ff5ccf72534f2af0f4fe52851ee6e3d6b2d3454
prestuuu <47060195+prestuuu@users.noreply.github.com>
2021-04-30 12:38:28 +0530

    Essential Changes + Mobile Navigation Added

--- models/product.js
+++ models/product.js
@@ -1,13 +1,13 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
 const p = path.join(
-  path.dirname(require.main.filename),
-  "data",
-  "products.json"
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
 );
 
-const getProductsFromFile = (cb) => {
+const getProductsFromFile = cb => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
       cb([]);
@@ -26,10 +26,10 @@ module.exports = class Product {
   }
 
   save() {
-    getProductsFromFile((products) => {
+    getProductsFromFile(products => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
-        // console.log(err);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
       });
     });
   }
