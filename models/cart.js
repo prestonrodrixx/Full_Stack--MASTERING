d68c80d7308ee17b57ef27729c678a1537f53cd4
prestuuu <47060195+prestuuu@users.noreply.github.com>
2021-05-02 23:55:14 +0530

    Implemented Delete Product from Cart Functionality

--- models/cart.js
+++ models/cart.js
@@ -45,14 +45,29 @@ module.exports = class Cart {
       }
       const updatedCart = { ...JSON.parse(fileContent) };
       const product = updatedCart.products.findIndex((prod) => prod.id === id);
+      if (!product) {
+        return;
+      }
       const productQty = product.qty;
       updatedCart.products = updatedCart.products.filter(
-        prod => prod.id !== id
+        (prod) => prod.id !== id
       );
-      updatedCart.totalPrice = updatedCart.totalPrice - productPrice * productQty;
+      updatedCart.totalPrice =
+        updatedCart.totalPrice - productPrice * productQty;
       fs.writeFile(p, JSON.stringify(updatedCart), (err) => {
         console.log(err);
+      });
     });
-  });
+  }
+
+  static getCart(cb) {
+    fs.readFile(p, (err, fileContent) => {
+      const cart = JSON.parse(fileContent);
+      if (err) {
+        cb(null);
+      } else {
+        cb(cart);
+      }
+    });
   }
 };
