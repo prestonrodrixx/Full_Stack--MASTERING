fb53614e34aea595fff1dcee47bfb60e4b37c332
prestuuu <47060195+prestuuu@users.noreply.github.com>
2021-05-06 19:56:35 +0530

    Project Reset

--- app.js
+++ app.js
@@ -1,38 +1,8 @@
-const path = require('path');
-
 const express = require('express');
-const bodyParser = require('body-parser');
-
-const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user');
-
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.use('/', (req, res, next) => {
+  res.send('Project Reset');
+})
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use((req, res, next) => {
-  User.findById('60937261f029c1203f5063b7')
-    .then(user => {
-      req.user = user;
-      next();
-    })
-    .catch(err => console.log(err));
-  next(); 
-});
-
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
-
-app.use(errorController.get404);
-
-mongoConnect(() => {
-  app.listen(3000);
-});
+app.listen(3000);
