525534dbf7ff7b00e4bc1abb363f273d64656132
prestuuu <47060195+prestuuu@users.noreply.github.com>
2021-05-09 11:50:44 +0530

    Register functionality Working

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
+const bcrypt = require('bcrypt');
 
-app.set('view-engine', 'ejs')
+const users = [];
+
+app.set('view-engine', 'ejs');
+
+// To get access from forms to get request from the post method
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.render('index.ejs', { name: 'Preston' });
@@ -11,12 +17,26 @@ app.get('/login', (req, res) => {
   res.render('login.ejs');
 });
 
+app.post('/login', (req, res) => {});
+
 app.get('/register', (req, res) => {
   res.render('register.ejs');
 });
 
-app.post('/register', (req, res) => {
-  
+app.post('/register', async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    res.redirect('/login');
+  } catch {
+    res.redirect('/register');
+  }
+  console.log(users);
 });
 
 app.listen(3000);
