3f3ab53caa34db1b48d47bf759e9445aab751d7f
prestuuu <47060195+prestuuu@users.noreply.github.com>
2021-05-22 15:01:32 +0530

    Added controllers folder

--- routes/users.js
+++ routes/users.js
@@ -3,27 +3,43 @@ import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
-const users = [];
+let users = [];
 
 router.get('/', (req, res) => {
-  console.log(users);
   res.send(users);
 });
 
-router.post('/', (req, res) => {
-  const user = req.body;
-  const userId = uuidv4();
-  const userWithId = { ...user, id: userId }; // Added ID to the User
-  users.push(userWithId);
-  res.send(`User with the name ${user.firstName} added.`);
-});
 
+
+// Creating a new user
+router.post('/', createUser);
+
+// Getting the user by id
 router.get('/:id', (req, res) => {
   const { id } = req.params;
   const foundUser = users.find((user) => user.id == id);
   res.send(foundUser);
 });
 
-router.delete('/:id',)
+// Deleting the user by id
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  users = users.filter((user) => user.id != id);
+  res.send(`User with id ${id} Deleted.`);
+});
+
+// Updating the user
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const { firstName, lastname, age } = req.body;
+
+  const user = users.find((user) => user.id == id);
+
+  if (firstName) user.firstName = firstName;
+  if (lastname) user.lastname = lastname;
+  if (age) user.age = age;
+
+  res.send(`User with id ${id} Updated.`);
+});
 
 export default router;
