b21baeafad3c016f5829425c35f944908c0f8f6c
prestuuu <47060195+prestuuu@users.noreply.github.com>
2021-05-24 11:37:54 +0530

    Mysql Connection Successful

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 import express from 'express';
 import ejs from 'ejs';
 import bodyParser from 'body-parser';
-import yoast from 'yoast';
+import yeast from 'yeast';
+import knex from 'knex';
 
 const app = express();
 
+const db = knex({
+  client: 'mysql',
+  connection: {
+    user:'root',
+    password:'mysqlpreston',
+    database:'url_shortner',
+    host:'localhost'
+  }
+});
+
 app.use(express.urlencoded({ extended: true }));
+
+app.post('/', (req, res) => {
+  const url = req.body.url;
+  db('urls').insert({
+    url,
+  }).then(ids => {
+    const alias = yeast.encode(ids[0]);
+    res.send(alias);
+  }).catch(console.log)
+});
 
 app.get('/', (req, res) => {
   ejs.renderFile(
@@ -19,9 +40,6 @@ app.get('/', (req, res) => {
   );
 });
 
-app.post('/', (req, res) => {
-  const url = req.body.url;
-  res.send(url);
-});
+
 
 app.listen(3000);
