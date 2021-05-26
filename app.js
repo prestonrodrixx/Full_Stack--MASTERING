a5b4b805a6ee635c261b8113de4e2f47a20ad2af
prestuuu <47060195+prestuuu@users.noreply.github.com>
2021-05-26 13:24:39 +0530

    Wrapped up with URL Shortner

--- app.js
+++ app.js
@@ -1,62 +1,16 @@
-import express from 'express';
-import ejs from 'ejs';
-import bodyParser from 'body-parser';
-import yeast from 'yeast';
-import knex from 'knex';
-
+const express = require('express');
 const app = express();
-
-const db = knex({
-  client: 'mysql',
-  connection: {
-    user: 'root',
-    password: 'mysqlpreston',
-    database: 'url_shortner',
-    host: 'localhost',
-  },
-});
-
-app.use(express.urlencoded({ extended: true }));
-
-app.get('/:alias', (req, res, next) => {
-  const alias = req.params.alias;
-  if (alias) {
-    db.select('url').from('urls')
-    .from('urls')
-    .where({id: yeast.decode(alias)})
-    .first()
-    .then(urlRow => {
-      res.redirect(urlRow.url);
-    })
-  } else {
-    next();
-  }
-});
+const PORT = 3000;
 
 app.get('/', (req, res) => {
-  const alias = req.query.alias;
-  ejs.renderFile(
-    'index.ejs',
-    {
-      alias,
-    },
-    function (err, html) {
-      res.send(html);
-    }
-  );
+  res.send('Root Directory');
 });
 
-app.post('/', (req, res) => {
-  const url = req.body.url;
-  db('urls')
-    .insert({
-      url,
-    })
-    .then((ids) => {
-      const alias = yeast.encode(ids[0]);
-      res.redirect('/?alias=' + alias);
-    })
-    .catch(console.log);
+app.use((req, res) => {
+  res.status(404).send('404 Page Not Found');
 });
 
-app.listen(3000);
+app.listen(PORT, (err) => {
+  if (err) console.log(err);
+  console.log('Server listening on PORT', PORT);
+});
---
+++ tiny_projects/url_shortner/app.js
@@ -0,0 +1,64 @@
+import express from 'express';
+import ejs from 'ejs';
+import bodyParser from 'body-parser';
+import yeast from 'yeast';
+import knex from 'knex';
+
+const app = express();
+
+const db = knex({
+  client: 'mysql',
+  connection: {
+    user: 'root',
+    password: 'mysqlpreston',
+    database: 'url_shortner',
+    host: 'localhost',
+  },
+});
+
+app.use(express.static('public'))
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/:alias', (req, res, next) => {
+  const alias = req.params.alias;
+  if (alias) {
+    db.select('url').from('urls')
+    .from('urls')
+    .where({id: yeast.decode(alias)})
+    .first()
+    .then(urlRow => {
+      res.redirect(urlRow.url);
+    })
+  } else {
+    next();
+  }
+});
+
+app.get('/', (req, res) => {
+  const alias = req.query.alias;
+  ejs.renderFile(
+    'index.ejs',
+    {
+      alias,
+    },
+    function (err, html) {
+      res.send(html);
+    }
+  );
+});
+
+app.post('/', (req, res) => {
+  const url = req.body.url;
+  db('urls')
+    .insert({
+      url,
+    })
+    .then((ids) => {
+      const alias = yeast.encode(ids[0]);
+      res.redirect('/?alias=' + alias);
+    })
+    .catch(console.log);
+});
+
+app.listen(3001);
