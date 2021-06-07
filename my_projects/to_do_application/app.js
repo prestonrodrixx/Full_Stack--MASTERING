564af5d91c96e5f7f46f82ed470cedb576d84b0c
prestuuu <47060195+prestuuu@users.noreply.github.com>
2021-06-07 18:46:00 +0530

    Added ejs and styles to it

--- my_projects/to_do_application/app.js
+++ my_projects/to_do_application/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 
+app.use('/static', express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('todo.ejs');
 });
 
 app.listen(3000, (err) => {
