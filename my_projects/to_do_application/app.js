e2f43d74ed70becb0e1f1b7d8403e8710c073e37
prestuuu <47060195+prestuuu@users.noreply.github.com>
2021-06-08 19:21:07 +0530

    Wrapped Up

--- my_projects/to_do_application/app.js
+++ my_projects/to_do_application/app.js
@@ -34,6 +34,35 @@ app.post('/', async (req, res) => {
   }
 });
 
+// UPDATE Method
+// First we find our id and we render the new template.
+// Then we update our task using the method findByIdAndUpdate.
+app
+  .route('/edit/:id')
+  .get((req, res) => {
+    const id = req.params.id;
+    TodoTask.find({}, (err, tasks) => {
+      res.render('todoEdit.ejs', { todoTasks: tasks, idTask: id });
+    });
+  })
+  .post((req, res) => {
+    const id = req.params.id;
+    TodoTask.findByIdAndUpdate(id, { content: req.body.content }, (err) => {
+      if (err) return res.send(500, err);
+      res.redirect('/');
+    });
+  });
+
+// DELETE Method
+// To do that we will use the method findByIdAndRemove.
+app.route('/remove/:id').get((req, res) => {
+  const id = req.params.id;
+  TodoTask.findByIdAndRemove(id, (err) => {
+    if (err) return res.send(500, err);
+    res.redirect('/');
+  });
+});
+
 // Connection to db
 mongoose.set('useFindAndModify', false);
 // DB_CONNECT credentials from .env file
