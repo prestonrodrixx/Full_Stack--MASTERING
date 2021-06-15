be69bdefb9c6c486a59d1a77eccef50d00669b8f
prestuuu <47060195+prestuuu@users.noreply.github.com>
2021-06-15 10:35:33 +0530

    Implemented Form

--- my_projects/cowin_notification/src/App.js
+++ my_projects/cowin_notification/src/App.js
@@ -1,5 +1,8 @@
 import logo2 from './logo2.svg';
 import './App.css';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
@@ -8,8 +11,65 @@ function App() {
         <h1 className="header-text">My Covid App</h1>
         <img src={logo2} className="App-logo" alt="logo"></img>
       </div>
-      <div className="text"></div>
-      <div className="container"></div>
+      <div className="text">
+        18+ and unable to book slot? No need to refresh the CoWIN website every
+        five minutes: Sign up and you will receive an email update when a slot
+        is available for you.
+      </div>
+      <div className="container">
+        <h2 className="title">Please fill the form...</h2>
+        <Form>
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+          </Form.Row>
+
+          <Form.Group controlId="formGridAddress1">
+            <Form.Label>Address</Form.Label>
+            <Form.Control placeholder="1234 Main St" />
+          </Form.Group>
+
+          <Form.Group controlId="formGridAddress2">
+            <Form.Label>Address 2</Form.Label>
+            <Form.Control placeholder="Apartment, studio, or floor" />
+          </Form.Group>
+
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridCity">
+              <Form.Label>City</Form.Label>
+              <Form.Control />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridState">
+              <Form.Label>State</Form.Label>
+              <Form.Control as="select" defaultValue="Choose...">
+                <option>Choose...</option>
+                <option>...</option>
+              </Form.Control>
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridZip">
+              <Form.Label>Zip</Form.Label>
+              <Form.Control />
+            </Form.Group>
+          </Form.Row>
+
+          <Form.Group id="formGridCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 }
