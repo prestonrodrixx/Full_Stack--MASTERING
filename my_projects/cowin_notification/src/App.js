5e7c49ee1ec2d8f2493f66f70cf23abca42eff9c
prestuuu <47060195+prestuuu@users.noreply.github.com>
2021-06-15 09:19:07 +0530

    Made it better

--- my_projects/cowin_notification/src/App.js
+++ my_projects/cowin_notification/src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+import logo2 from './logo2.svg';
 import './App.css';
 
 function App() {
   return (
     <div style={{ maxWidth: '500px' }}>
-      <div className="heading"></div>
+      <div className="heading">
+        <h1 className="header-text">My Covid App</h1>
+        <img src={logo2} className="App-logo" alt="logo"></img>
+      </div>
       <div className="text"></div>
       <div className="container"></div>
     </div>
