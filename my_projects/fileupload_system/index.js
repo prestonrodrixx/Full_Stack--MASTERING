89a299c53f8a00bb6bfd4f1fc79e710dd26f7c46
prestuuu <47060195+prestuuu@users.noreply.github.com>
2021-06-22 16:25:48 +0530

    Wrapped up with file upload

--- my_projects/fileupload_system/index.js
+++ my_projects/fileupload_system/index.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const upload = require('express-fileupload');
+const path = require('path');
 
 const app = express();
 
-app.use(upload);
+app.use(express.urlencoded({ extended: true }));
+
+app.use(upload());
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', (req, res) => {
+  // Accessing the file by the <input> File name="file"
+  let targetFile = req.files.file;
+
+  //mv(path, CB function(err))
+  targetFile.mv(path.join(__dirname, 'uploads', targetFile.name), (err) => {
+    if (err) return res.status(500).send(err);
+    res.send('File uploaded!');
+  });
 });
 
 app.listen(3000);
