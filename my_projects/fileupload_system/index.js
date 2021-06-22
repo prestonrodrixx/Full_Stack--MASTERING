80ea812058264e91f51a2cb5be9390ccfc043c96
prestuuu <47060195+prestuuu@users.noreply.github.com>
2021-06-22 15:47:29 +0530

    Made it better

---
+++ my_projects/fileupload_system/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const upload = require('express-fileupload');
+
+const app = express();
+
+app.use(upload);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(3000);
