c6a23d25763f4e0e38b35a570bc025036faaadfd
prestuuu <47060195+prestuuu@users.noreply.github.com>
2021-06-23 18:50:19 +0530

    Added conditions to users to view specific routes

--- my_projects/manage_user_roles/routes/projects.js
+++ my_projects/manage_user_roles/routes/projects.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const { projects } = require('../data');
+const { authUser, authRole } = require('../basicAuth');
+const { canViewProject } = require('../permissions/project');
 
 router.get('/', (req, res) => {
   res.json(projects);
 });
 
-router.get('/:projectId', setProject, (req, res) => {
+router.get('/:projectId', setProject, authUser, authGetProject, (req, res) => {
   res.json(req.project);
 });
 
@@ -17,6 +19,14 @@ function setProject(req, res, next) {
   if (req.project == null) {
     res.status(404);
     return res.send('Project not found');
+  }
+  next();
+}
+
+function authGetProject(req, res, next) {
+  if (!canViewProject(req.user, req.project)) {
+    res.status(401);
+    return res.send('Not Allowed');
   }
   next();
 }
