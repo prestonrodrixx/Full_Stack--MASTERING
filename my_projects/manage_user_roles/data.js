69eb142f6ef34f5e90d672663892584453b18282
prestuuu <47060195+prestuuu@users.noreply.github.com>
2021-06-23 18:24:25 +0530

    BAsic auth for dashboard

--- my_projects/manage_user_roles/data.js
+++ my_projects/manage_user_roles/data.js
@@ -1,18 +1,18 @@
 const ROLE = {
   ADMIN: 'admin',
-  BASIC: 'basic'
-}
+  BASIC: 'basic',
+};
 
 module.exports = {
   ROLE: ROLE,
   users: [
-    { id: 1, name: 'Kyle', role: ROLE.ADMIN },
-    { id: 2, name: 'Sally', role: ROLE.BASIC },
-    { id: 3, name: 'Joe', role: ROLE.BASIC }
+    { id: 1, name: 'Preston', role: ROLE.ADMIN },
+    { id: 2, name: 'Aaron', role: ROLE.BASIC },
+    { id: 3, name: 'Nelson', role: ROLE.BASIC },
   ],
   projects: [
-    { id: 1, name: "Kyle's Project", userId: 1 },
-    { id: 2, name: "Sally's Project", userId: 2 },
-    { id: 3, name: "Joe's Project", userId: 3 }
-  ]
-}
+    { id: 1, name: "Preston's Project", userId: 1 },
+    { id: 2, name: "Aaron's Project", userId: 2 },
+    { id: 3, name: "Nelson's Project", userId: 3 },
+  ],
+};
---
+++ my_projects/manage_user_roles/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const { users } = require('./data');
+const { authUser } = require('./basicAuth');
+const projectRouter = require('./routes/projects');
+
+app.use(express.json());
+app.use(setUser);
+app.use('/projects', projectRouter);
+
+app.get('/', (req, res) => {
+  res.send('Home Page');
+});
+
+app.get('/dashboard', authUser, (req, res) => {
+  res.send('Dashboard Page');
+});
+
+app.get('/admin', (req, res) => {
+  res.send('Admin Page');
+});
+
+function setUser(req, res, next) {
+  const userId = req.body.userId;
+  if (userId) {
+    req.user = users.find((user) => user.id === userId);
+  }
+  next();
+}
+
+app.listen(3000, () => {
+  console.log('Server Started');
+});
