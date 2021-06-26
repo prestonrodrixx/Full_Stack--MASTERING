688c796909cebf04fa79ff9f923629c75d2bd12a
prestuuu <47060195+prestuuu@users.noreply.github.com>
2021-06-26 10:35:54 +0530

    Added card

--- my_projects/material_ui_react/src/App.jsx
+++ my_projects/material_ui_react/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Typography, AppBar, Button, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core';
 import { PhotoCamera } from '@material-ui/icons';
-
 import useStyles from './styles';
 
 const App = () => {
@@ -11,7 +10,7 @@ const App = () => {
             <CssBaseline />
             <AppBar position="relative">
                 <Toolbar>
-                    <PhotoCamera />
+                    <PhotoCamera className={classes.icon} />
                     <Typography variant="h6">
                         Photo Album
                     </Typography>
@@ -27,18 +26,30 @@ const App = () => {
                         <Typography variant="h5" align="center" color="textSecondary" paragraph>
                             Hello and welcome to my photo album. This is practice react application using material ui.
                         </Typography>
-                        <div>
+                        <div className={classes.button}>
                             <Grid container spacing={2} justify="center">
                                 <Grid item>
                                     <Button variant="contained" color="primary">View Photos</Button>
                                 </Grid>
                                 <Grid item>
-                                    <Button variant="outlined" color="primary">Download</Button>
+                                    <Button variant="outlined" color="primary">Downloads</Button>
                                 </Grid>
                             </Grid>
                         </div>
                     </Container>
                 </div>
+                <Container className={classes.cardGrid} maxWidth="md">
+                    <Grid container spacing={4}>
+                        <Grid item>
+                            <Card className={classes.card}>
+                                <CardMedia className={classes.cardMedia} image="https://source.unsplash.com/random" title="Image Title"/>
+                                <CardContent className={classes.cardContent}>
+                                    <Typography gutterBottom variant="h5">Hello</Typography>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                    </Grid>
+                </Container>
             </main>
         </>
     );
