8b7a435c130545d6daa4e8964530eb787e136ac2
prestuuu <47060195+prestuuu@users.noreply.github.com>
2021-06-29 09:19:45 +0530

    Working on Side Menu

---
+++ my_projects/material_ui_react_advanced/src/App/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import SideMenu from '../components/SideMenu';
+
+function App() {
+  return (
+    <>
+      <SideMenu />
+      <div>Here we go</div>
+    </>
+  );
+}
+
+export default App;
