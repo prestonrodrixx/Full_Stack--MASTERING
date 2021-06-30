6a6667d8c596380acf0076b72dfebbea913f7d51
prestuuu <47060195+prestuuu@users.noreply.github.com>
2021-06-30 13:43:00 +0530

    Better search field

--- my_projects/material_ui_react_advanced/src/components/Header.js
+++ my_projects/material_ui_react_advanced/src/components/Header.js
@@ -7,34 +7,55 @@ import {
   InputBase,
   IconButton,
   Badge,
+  makeStyles,
 } from '@material-ui/core';
 import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
 import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
 import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
+import SearchIcon from '@material-ui/icons/Search';
+
+const useStyle = makeStyles({
+  root: {
+    backgroundColor: '#fff',
+  },
+  searchInput: {
+    opacity: '0.6',
+    padding: '0px 8px',
+    fontSize: '0.8rem',
+    '&:hover': {
+      backgroundColor: '#f2f2f2',
+    },
+  },
+});
 
 export default function Header() {
+  const classes = useStyle();
   return (
     <div>
-      <AppBar position="static">
+      <AppBar position="static" className={classes.root}>
         <Toolbar>
-          <Grid container>
+          <Grid container alignItems="center">
             <Grid item>
-              <InputBase />
+              <InputBase
+                placeholder="Search topics"
+                className={classes.searchInput}
+                startAdornment={<SearchIcon fontSize="small" />}
+              />
             </Grid>
             <Grid item sm></Grid>
             <Grid item>
               <IconButton>
                 <Badge badgeContent={4} color="secondary">
-                  <NotificationsNoneIcon />
+                  <NotificationsNoneIcon fontSize="small" />
                 </Badge>
               </IconButton>
               <IconButton>
-                <Badge badgeContent={3} color="secondary">
-                  <ChatBubbleOutlineIcon />
+                <Badge badgeContent={3} color="primary">
+                  <ChatBubbleOutlineIcon fontSize="small" />
                 </Badge>
               </IconButton>
               <IconButton>
-                <PowerSettingsNewIcon />
+                <PowerSettingsNewIcon fontSize="small" />
               </IconButton>
             </Grid>
           </Grid>
